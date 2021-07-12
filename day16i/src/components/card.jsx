df958d19b78b6e424e1ce3c208d57052ea2f9f44
Ashmita715 <ashishukla2902@gmail.com>
2021-07-12 13:33:59 +0530

    Add day 16

---
+++ day16i/src/components/card.jsx
@@ -0,0 +1,13 @@
+const Card = () => {
+    return(
+        <div className="card">
+            <img
+            src="https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/c87n1atdrnllejdk_1604929126.jpeg" 
+            alt="" />
+            <h2>React Card</h2>
+            <p>This is a card</p>
+
+        </div>
+    );
+};
+export default Card; 
