7cf780865edc493efec465eac0c1fb15586d350c
Ashmita715 <ashishukla2902@gmail.com>
2021-07-13 17:50:02 +0530

    Add day18

---
+++ day18/src/components/Card1.jsx
@@ -0,0 +1,9 @@
+const Card1 = () => {
+    return (
+        <div className="card1" style={{backgroundColor: "white", padding: '0px', margin: '0px', height: '25px', width: '25px'}}>
+        </div>
+
+    );
+};
+
+export default Card1;
