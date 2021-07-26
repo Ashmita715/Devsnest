d26ab69a91454a5b1af4ac04e1f62ab6576e5434
Ashmita Shukla <ashishukla2902@gmail.com>
2021-07-26 18:17:05 +0530

    Add files

--- day18/src/components/Card1.jsx
+++
@@ -1,9 +0,0 @@
-const Card1 = () => {
-    return (
-        <div className="card1" style={{backgroundColor: "white", padding: '0px', margin: '0px', height: '25px', width: '25px'}}>
-        </div>
-
-    );
-};
-
-export default Card1;
